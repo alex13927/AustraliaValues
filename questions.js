6a5dd5532b71998204f2060e202b05d5744487f6
alex13927 <74085892+alex13927@users.noreply.github.com>
2021-01-27 03:23:04 +1100

    Update questions.js

--- questions.js
+++ questions.js
@@ -9,12 +9,30 @@ questions = [
         }
     },
     {
-        "question": "It is necessary for the government to intervene in the economy to protect consumers.",
+        "question": "The freer the markets, the freer the people.",
         "effect": {
-            "econ": 10,
+            "econ": -10,
             "dipl": 0,
             "govt": 0,
             "scty": 0
+        }
+    },
+    {
+        "question": "Free trade is preferable to protectionism.",
+        "effect": {
+            "econ": -10,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Voting in elections should be voluntary, not compulsory.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
         }
     }
 ];
